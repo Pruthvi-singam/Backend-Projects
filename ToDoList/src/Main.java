90a68f8614dd9cb39006a4d5aeca71b7eea5448a
pruthvi-singam <pruthvisingamasetti@gmail.com>
2025-01-21 13:31:18 -0500

    delete function added

--- ToDoList/src/Main.java
+++ ToDoList/src/Main.java
@@ -15,6 +15,28 @@ public class Main {
             System.out.println(e.getMessage());
         }
     }
+    public static void update(List<Task> tasks,int id)
+    {
+
+        for(Task tas:tasks) {
+            if (tas.getId() == 1) {
+                tas.setDescription("this is  new changed Description 2");
+
+            }
+
+        }
+    }
+    public static void deleteTask(List<Task> tasks,int id)
+    {
+        tasks.removeIf(task-> {
+            if(task.getId()==id)
+            {
+                return true;
+            }
+            return false;
+        });
+
+    }
     public static void listTasks(List<Task> tasks)
     {
         for(Task task:tasks)
@@ -30,21 +52,28 @@ public class Main {
         String status2 = "Done";
         String status3 = "ToDo";
 
-        validateStatus(status1);
-        validateStatus(status2);
-        validateStatus(status3);
+//        validateStatus(status1);
+//        validateStatus(status2);
+//        validateStatus(status3);
 //
 //        Task newTask=new Task(1,"something","In_progress");
 //        Task newTask1=new Task(2,"Buy Groceries 1","In_progress");
 //        Task newTask2=new Task(3,"Buy Groceries 2","Done");
 //        Task newTask3=new Task(4,"Buy Groceries 3","ToDo");
-
+//
 //        tasks.add(newTask);
 //        tasks.add(newTask1);
 //        tasks.add(newTask2);
 //        tasks.add(newTask3);
+
         FileUtil fu=new FileUtil();
-        fu.saveTaskToFile(tasks);
+//        fu.saveTaskToFile(tasks);
+        listTasks(tasks);
+//        System.out.println();
+//        update(tasks,1);
+        deleteTask(tasks,3);
+//        fu.saveTaskToFile(tasks);
+//        System.out.println();
         listTasks(tasks);
     }
 
--- ToDoList/src/Task.java
+++ ToDoList/src/Task.java
@@ -65,6 +65,6 @@ public class Task
     public String toString()
     {
 
-        return "Task{id=" +id+",name='"+Description+"Status="+status+"'}";
+        return "Task{id=" +id+",name='"+Description+"' ,Status='"+status+"'}";
     }
 }
